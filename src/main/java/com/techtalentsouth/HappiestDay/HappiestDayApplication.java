4b08fe0c644b3c5e2c587c46af6a525cb9829d5a
Ben <allredbm@yahoo.com>
2018-12-14 16:09:24 -0500

    Initial Commit

---
+++ src/main/java/com/techtalentsouth/HappiestDay/HappiestDayApplication.java
@@ -0,0 +1,14 @@
+package com.techtalentsouth.HappiestDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappiestDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappiestDayApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/techtalentsouth/HappiestDay/HappiestDayApplicationTests.java
@@ -0,0 +1,17 @@
+package com.techtalentsouth.HappiestDay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HappiestDayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
