82541b9d8758203fc0dda413c420366a4ffa495e
Ben <allredbm@yahoo.com>
2018-12-14 23:31:06 -0500

    Create Attendee repository

---
+++ src/main/java/com/techtalentsouth/HappiestDay/Attendee/AttendeeRepository.java
@@ -0,0 +1,7 @@
+package com.techtalentsouth.HappiestDay.Attendee;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttendeeRepository extends CrudRepository<Attendee, Long>{
+
+}
