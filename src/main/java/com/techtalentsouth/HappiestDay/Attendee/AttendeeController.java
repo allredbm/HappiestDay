e5e01424bfd94e2c930bb7b669dbc3f2d7085371
Ben <allredbm@yahoo.com>
2018-12-18 12:42:32 -0500

    Update index.html to use Attendee package

--- src/main/java/com/techtalentsouth/HappiestDay/Attendee/AttendeeController.java
+++ src/main/java/com/techtalentsouth/HappiestDay/Attendee/AttendeeController.java
@@ -3,6 +3,7 @@ package com.techtalentsouth.HappiestDay.Attendee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,8 +13,18 @@ public class AttendeeController {
 	private AttendeeRepository attendeeRepository;
 	
 	@GetMapping("/")
-	public ModelAndView index() {
+	public ModelAndView index(Attendee attendee) {
 		ModelAndView mv = new ModelAndView("Attendee/index.html");
+		mv.addObject("attendees", attendeeRepository.findAll());
+		
+		return mv;
+	}
+	
+	@PostMapping(value="/attendee")
+	public ModelAndView addNewUser(Attendee attendee) {
+		attendeeRepository.save(attendee);
+		ModelAndView mv = new ModelAndView("Attendee/index.html");
+		mv.addObject("attendees", attendeeRepository.findAll());
 		
 		return mv;
 	}
